6aa27cf91f4d5a9144f794e7f1981f47ebacbfc0
Jan Nowak <nowak.jan@yahoo.com>
2023-04-15 21:57:16 +0200

    add function that finds cart with highest value

---
+++ findHighestCartValue.js
@@ -0,0 +1,32 @@
+module.exports = findHighestCartValue = (users, carts, products) => {
+  try {
+    let newCarts = carts.map((cart) => {
+      let totalValue = countTotalValue(products, cart);
+      const fullName = createFullName(users, cart);
+      return { ...cart, totalValue: totalValue, fullName: fullName };
+    });
+    const resultCart = findMaxCart(newCarts);
+    return resultCart;
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
+};
+
+const createFullName = (users, cart) => {
+  const user = users.find((user) => user.id === cart.userId);
+  return user.name.firstname + " " + user.name.lastname;
+};
+
+const countTotalValue = (products, cart) => {
+  return cart.products.reduce(
+    (sum, current) =>
+      sum + products.find((product) => product.id === current.productId).price,
+    0
+  );
+};
+
+const findMaxCart = (carts) => {
+  return carts.reduce((max, cart) =>
+    max.totalValue > cart.totalValue ? max : cart
+  );
+};
